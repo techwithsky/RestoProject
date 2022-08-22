8a0af97c44471a407e4fa15a38b836f434fb99c0
Akashpawar11 <97218796+Akashpawar11@users.noreply.github.com>
2022-08-22 22:01:09 +0530

    Delete vue_self directory

--- vue_self/vue.config.js
+++
@@ -1,6 +0,0 @@
-// const bootstrapSassAbstractsImports = require('vue-cli-plugin-bootstrap-vue/sassAbstractsImports.js')
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
-;
